43f93f3ddfe12737656385af4d9b84ec5af5cfd4
Ezar Surahman <akhdanezar@gmail.com>
2024-07-30 15:56:44 +0700

    First commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Ezar Akhdan Shada Surahman" // TODO: replace this
+const studentId = "idontknow" // TODO: replace this
+
+export { url, studentName, studentId }
